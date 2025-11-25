25ed2581ea0da3b4a0d7b2b39df6e2b962a36751
Andrew <hjrrrb16@gmail.com>
2025-11-25 23:45:17 +0200

    Практична робота №1

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout'; 
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, LayoutComponent], 
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title = 'my-angular-app';
+}
